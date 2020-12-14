01e0e033528e950a36ccfcfa900233f3957a3ca3
guintoki <guilherme-vilas-boas@hotmail.com>
2020-12-13 23:08:31 -0300

    fix provider style

--- src/Pages/Dashboard/styles.ts
+++ src/Pages/Dashboard/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { FlatList } from 'react-native';
+import { Provider } from './index';
 
 export const Container = styled.View`
   flex: 1;
@@ -27,9 +29,12 @@ export const UserName = styled.Text`
   font-family: 'RobotoSlab-Medium';
 `;
 
-export const ProfileButton = styled.TouchableOpacity;
+export const ProfileButton = styled.TouchableOpacity``;
+
 export const UserAvatar = styled.Image`
   width: 56px;
   height: 56px;
   border-radius: 28px;
 `;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)``;
