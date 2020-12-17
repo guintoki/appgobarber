9ce0ce10d9ad4b24608f71da891efa12c929a653
guintoki <guilherme-vilas-boas@hotmail.com>
2020-12-16 22:34:36 -0300

    addproviders list components

--- src/Pages/CreateAppointment/index.tsx
+++ src/Pages/CreateAppointment/index.tsx
@@ -1,17 +1,30 @@
+/* eslint-disable camelcase */
 import { useNavigation, useRoute } from '@react-navigation/native';
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
 import {
   Container,
   Header,
   BackButton,
   HeaderTitle,
+  ProvidersListContainer,
   UserAvatar,
+  ProvidersList,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderName,
 } from './styles';
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
 
 interface RouteParams {
   providerId: string;
+}
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
 }
 
 const CreateAppointment: React.FC = () => {
@@ -20,9 +33,17 @@ const CreateAppointment: React.FC = () => {
   const { goBack } = useNavigation();
   const { providerId } = route.params as RouteParams;
 
+  const [providers, setProviders] = useState<Provider[]>([]);
+
   const navigateBack = useCallback(() => {
     goBack();
   }, [goBack]);
+
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
 
   return (
     <Container>
@@ -35,6 +56,21 @@ const CreateAppointment: React.FC = () => {
 
         <UserAvatar source={{ uri: user.avatar_url }} />
       </Header>
+
+      <ProvidersListContainer>
+        <ProvidersList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={providers}
+          keyExtractor={provider => provider.id}
+          renderItem={({ item: provider }) => (
+            <ProviderContainer>
+              <ProviderAvatar source={{ uri: provider.avatar_url }} />
+              <ProviderName>{provider.name}</ProviderName>
+            </ProviderContainer>
+          )}
+        />
+      </ProvidersListContainer>
     </Container>
   );
 };
