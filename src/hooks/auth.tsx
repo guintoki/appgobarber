80570327c596d4794b4f7e472fd6a6520ff6c847
guintoki <guilherme-vilas-boas@hotmail.com>
2020-12-20 18:52:55 -0300

    add update profile

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -33,6 +33,7 @@ interface AuthContextData {
   user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
+  updateUser(user: User): Promise<void>;
   loading: boolean;
 }
 
@@ -84,9 +85,22 @@ export const AuthProvider: React.FC = ({ children }) => {
 
     setData({} as AuthState);
   }, []);
+  const updateUser = useCallback(
+    async (user: User) => {
+      await AsyncStorage.setItem('@Gobarber:user', JSON.stringify(user));
+
+      setData({
+        token: data.token,
+        user,
+      });
+    },
+    [setData, data.token],
+  );
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut, loading }}>
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, loading, updateUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
