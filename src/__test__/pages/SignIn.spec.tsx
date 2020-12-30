53d9857d0fae8b0ab4a8495064b6d5757ff89868
guintoki <guilherme-vilas-boas@hotmail.com>
2020-12-29 23:06:38 -0300

    add initial tests

---
+++ src/__test__/pages/SignIn.spec.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { render } from 'react-native-testing-library';
+import SignIn from '../../Pages/SignIn';
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    useNavigation: jest.fn(),
+  };
+});
+
+describe('SignIn page', () => {
+  it('should container email/password inputs', () => {
+    const { getByPlaceholder } = render(<SignIn />);
+
+    expect(getByPlaceholder('E-mail')).toBeTruthy();
+    expect(getByPlaceholder('Senha')).toBeTruthy();
+  });
+});
